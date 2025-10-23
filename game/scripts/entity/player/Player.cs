927504e9b8f944140f54b14e7dcc0576150bcbe1
Ramsay <floffah@floffah.dev>
2025-10-23 16:47:15 +0100

    feat: orbit camera

--- game/scripts/entity/player/Player.cs
+++ game/scripts/entity/player/Player.cs
@@ -2,15 +2,17 @@ using Godot;
 using System;
 
 public partial class Player : CharacterBody3D {
-    public const float Speed = 5.0f;
-    public const float JumpVelocity = 4.5f;
+    [Export] public float Speed = 5.0f;
+    [Export] public float JumpVelocity = 4.5f;
+    [Export] public float RotateSpeed = 10.0f;
+
+    [Export] public PlayerOrbitCamera CameraRig;
+    [Export] public float CameraDistanceY = 1f;
 
     private AnimationPlayer characterAnimations;
-    private Marker3D cameraPivot;
 
     public override void _Ready() {
         characterAnimations = GetNode<AnimationPlayer>("blockbench_export/AnimationPlayer");
-        cameraPivot = GetNode<Marker3D>("Pivot");
     }
 
     public override void _PhysicsProcess(double delta) {
@@ -29,7 +31,20 @@ public partial class Player : CharacterBody3D {
         // Get the input direction and handle the movement/deceleration.
         // As good practice, you should replace UI actions with custom gameplay actions.
         Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forwards", "move_backwards");
-        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+
+        // Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        Vector3 direction =
+            ((CameraRig.GlobalTransform.Basis.X * inputDir.X) + (CameraRig.GlobalTransform.Basis.Z * inputDir.Y))
+            .Normalized();
+
+        if (direction.Length() > 0.1f) {
+            var targetAngle = Mathf.Atan2(direction.X, direction.Z);
+            var smoothedAngle = Mathf.LerpAngle(Rotation.Y, targetAngle, delta * RotateSpeed);
+            SetRotation(
+                new Vector3(0, (float)smoothedAngle, 0)
+            );
+        }
+
         if (direction != Vector3.Zero) {
             velocity.X = direction.X * Speed;
             velocity.Z = direction.Z * Speed;
@@ -41,6 +56,8 @@ public partial class Player : CharacterBody3D {
 
         Velocity = velocity;
         MoveAndSlide();
+
+        CameraRig.Position = GlobalPosition + new Vector3(0, CameraDistanceY, 0);
 
         if (Mathf.Abs(Velocity.X) > 0.1f || Mathf.Abs(Velocity.Z) > 0.1f) {
             characterAnimations.Play("walk");
