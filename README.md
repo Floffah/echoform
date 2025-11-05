19b2acf56882b13b58d1f9092b02b7c7b2bfca3a
Ramsay <floffah@floffah.dev>
2025-11-05 17:50:57 +0000

    feat: global documentation & git lfs (#16)

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ See [`LICENSE`](./LICENSE) for full terms.
 
 My attempt at making an MMO game using Godot and Bun.
 
+> [!NOTE]
+> I am looking for artists to help out with graphics & 3d art 🙂, email echoform@ramdta.com if interested
+
 Packages:
 
 - [backend](./backend) - (Bun) The authoritative server for the game. Manages player profiles, load balancing, and realtime
---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,58 @@
+import { type DefaultTheme, defineConfig } from "vitepress";
+import { withMermaid } from "vitepress-plugin-mermaid";
+
+// https://vitepress.dev/reference/site-config
+export default withMermaid({
+    title: "Echoform Authoritative Docs",
+    description:
+        "Technical documentation for the Echoform game authoritative server",
+    appearance: true,
+    lastUpdated: true,
+
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            { text: "Game", link: "/game" },
+            { text: "Backend Reference", link: "/backend-reference" },
+        ],
+
+        sidebar: {
+            "/backend-reference": [
+                {
+                    text: "Flows",
+                    link: "/backend-reference/flows",
+                    items: [
+                        {
+                            text: "Authentication",
+                            link: "/backend-reference/flows/authentication",
+                        },
+                    ],
+                },
+            ],
+            "/game": [
+                {
+                    text: "Game",
+                    link: "/game",
+                    items: [
+                        {
+                            text: "Story",
+                            link: "/game/story",
+                        },
+                        {
+                            text: "Music",
+                            link: "/game/music",
+                        },
+                    ],
+                }
+            ],
+        },
+
+        socialLinks: [
+            { icon: "github", link: "https://github.com/floffah/echoform" },
+        ],
+
+        editLink: {
+            pattern: 'https://github.com/floffah/echoform/edit/main/docs/:path'
+        },
+    } as DefaultTheme.Config,
+});
